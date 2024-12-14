bb42a238801f7dfb8c48ae04f6ad34655b46bde1
Bipin Nair Gopala Krishnan <96311075+hykuBipin@users.noreply.github.com>
2024-12-14 13:05:17 +0530

    Time: 2 ms (89.32%), Space: 55.4 MB (60.15%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -6,15 +6,12 @@ class Solution {
         ans[0] = 1; 
         for (int i = 1; i < N; i++) {
             ans[i] = ans[i - 1] * nums[i - 1];
-            System.out.println(ans[i] + " "+ ans[i-1]+ " " + nums[i]+ " "+ nums[i-1]);
         }
 
         int rightProduct = 1;
         for (int i = N - 1; i >= 0; i--) {
             ans[i] *= rightProduct;
-             System.out.println(ans[i] + " " + rightProduct);
             rightProduct *= nums[i]; 
-             System.out.println(nums[i] + " " + rightProduct);
         }
 
         return ans;
