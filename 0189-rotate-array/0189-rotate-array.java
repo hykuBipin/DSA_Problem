1ff0245a2e49aafa991e073ca7db71c4682d5a33
Bipin Nair Gopala Krishnan <96311075+hykuBipin@users.noreply.github.com>
2025-07-07 14:33:20 +0530

    Time: 204 ms (5.22%), Space: 57.3 MB (66.03%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,7 +1,11 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n=nums.length;
-        k=k%n;
+        if(nums==null || n==0) 
+        {
+            throw new IllegalArgumentException("Array should not be null or empty");
+        }
+        k = k % n;
         reverse(nums,0,n-1);
         reverse(nums,0,k-1);
         reverse(nums,k,n-1);
@@ -9,11 +13,11 @@ class Solution {
 
     public static void reverse(int[] nums, int start, int end)
     {
-        while(start < end )
+        while(start < end)
         {
-            int temp = nums[start];
+            int temp=nums[start];
             nums[start]=nums[end];
-            nums[end]=temp; 
+            nums[end]=temp;
             start++;
             end--;
         }
