126a9c98c22002a88d15c526b7246e402a61a0a9
Bipin Nair Gopala Krishnan <96311075+hykuBipin@users.noreply.github.com>
2025-08-31 13:22:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
@@ -0,0 +1,28 @@
+class Solution {
+    int lowerBound(int[] arr, int target) {
+       int n=arr.length;
+       int low=0;
+       int high=n-1;
+       if(n==0 || arr==null)
+       {
+           throw new IllegalArgumentException("Array is null or empty");
+       }
+        int ans=n;
+       
+       while(low <= high)
+       {
+           int mid=low + (high-low)/2;
+           if(arr[mid] >= target)
+           {
+               //move left
+               ans=mid;
+               high=mid-1;
+           }else
+           {
+               //move right
+               low=mid+1;
+           }
+       }
+       return ans;
+    }
+}
