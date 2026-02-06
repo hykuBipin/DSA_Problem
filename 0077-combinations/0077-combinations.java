2f5e61e38d232dd7ed7a2dbe80fefc9ddd6c8110
Bipin Nair Gopala Krishnan <96311075+hykuBipin@users.noreply.github.com>
2026-02-06 10:15:43 +0530

    Time: 24 ms (16.35%), Space: 98.1 MB (63.16%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,33 @@
+class Solution {
+    List<List<Integer>> answer= new ArrayList<>();
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> result = new ArrayList<>();
+
+        solve(n,k,1,result);
+        return answer;
+    }
+
+    void solve(int n, int k, int start, List<Integer> result)
+    {
+        if(n<0) return;
+
+        if(result.size()==k) 
+        {
+            answer.add(new ArrayList<>(result));
+            return;
+        }
+
+        for(int i=start;i<=n;i++)
+        {
+
+            //avoid duplicates
+            if(i>0 && i==i-1) continue;
+
+            result.add(i);
+
+            solve(n,k,i+1,result);
+
+            result.remove(result.size()-1);
+        }
+    }
+}
