154a3a53f269d3f81073fd8dc856f28def3ed67c
Bipin Nair Gopala Krishnan <96311075+hykuBipin@users.noreply.github.com>
2026-03-07 13:15:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    static void rotateArr(int arr[], int d) {
+        int n=arr.length;
+        if(arr==null ||n <= 0) return;
+         d = d % n;
+        
+          swap(arr,0,d-1);
+          swap(arr,d,n-1);
+          swap(arr,0,n-1);
+    }
+    
+    public static void swap(int[] arr, int start, int end)
+    {
+        while(start < end)
+        {
+            int temp = arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
