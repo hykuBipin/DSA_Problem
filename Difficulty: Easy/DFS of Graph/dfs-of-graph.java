b7a87177b5f974ea4e22745d1117cc3bdf57b7ce
Bipin Nair Gopala Krishnan <96311075+hykuBipin@users.noreply.github.com>
2026-03-08 18:24:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+      boolean[] visited = new boolean[adj.size()];
+      ArrayList<Integer> res = new ArrayList<>();
+      
+      for(int i=0;i<adj.size();i++)
+      {
+          if(!visited[i])
+          {
+              dfssolve(adj,visited,0,res);
+          }
+      }
+      return res;
+        
+    }
+    
+    public static void dfssolve(ArrayList<ArrayList<Integer>> adj, boolean[] visited, int index, ArrayList<Integer> res)
+    {
+        visited[index]=true;
+        res.add(index);
+        
+        for(int i: adj.get(index))
+        {
+            if(!visited[i])
+            {
+                dfssolve(adj,visited,i,res);
+            }   
+        }
+    }
+}
