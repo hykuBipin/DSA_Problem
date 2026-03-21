f81e26a927a7a08050963a54a0a9b61b8bcde107
Bipin Nair Gopala Krishnan <96311075+hykuBipin@users.noreply.github.com>
2026-03-21 21:52:29 +0530

    Time: 0 ms (100.00%) | Memory: 61.2 MB (66.59%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if(nums==null) return;
+        int n=nums.length;
+        
+        k = k%n;
+
+        swap(nums,0,n-1);
+        swap(nums,0,k-1);
+        swap(nums,k,n-1);
+    }
+
+    public static void swap(int[] nums, int start, int end)
+    {
+        while(start<end)
+        {
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
