80e7aaf7777ddcfdad58fd0aae8dbcd68c7db644
Bipin Nair Gopala Krishnan <96311075+hykuBipin@users.noreply.github.com>
2026-03-21 14:02:48 +0530

    Time: 2 ms (99.24%) | Memory: 44.2 MB (82.40%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s==null) return true;
+        
+        int n=s.length();
+        if(n<=1) return true;
+        
+        int start=0,end=s.length()-1;
+        while(start<end)
+        {
+            while(start < end && !Character.isLetterOrDigit(s.charAt(start)))
+            {
+                start++;
+            }
+            
+            while(start < end && !Character.isLetterOrDigit(s.charAt(end)))
+            {
+                end--;
+            }
+
+            if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end)))
+            {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
