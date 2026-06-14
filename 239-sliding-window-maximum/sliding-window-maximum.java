b26d601135d64515b59eee9411d1c96e10d56a80
Bipin Nair Gopala Krishnan <96311075+hykuBipin@users.noreply.github.com>
2026-06-15 00:35:27 +0530

    Time: 31 ms (55.43%) | Memory: 146.7 MB (58.52%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums==null || nums.length <=0 || k==0 || k > nums.length) return new int[]{};
+        int n =nums.length;
+        int[] result =new int[n-k+1];
+        
+        Deque<Integer> dq = new ArrayDeque<>();
+        int idx=0;
+
+        for(int i=0;i<n;i++)
+        {
+            while( !dq.isEmpty() && dq.peekFirst() <= i-k)
+            {
+                dq.pollFirst();
+            }
+
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i])
+            {
+                dq.pollLast();
+            }
+
+            dq.offerLast(i);
+
+            if( i >= k-1)
+            {
+              result[idx++] = nums[dq.peekFirst()];
+            }
+        }
+        return result;
+    }
+}
+
+// Complexity:
+// TC: O(n)
+// SC: O(k)
